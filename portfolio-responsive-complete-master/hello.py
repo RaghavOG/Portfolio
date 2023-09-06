cd6f6d12367bee79fd17a778e93cc92c4d8389b2
RaghavOG <04raghavsingla28@gmail.com>
2023-09-06 11:48:28 +0530

    first commit

---
+++ portfolio-responsive-complete-master/hello.py
@@ -0,0 +1,78 @@
+# b = 111
+# # if b < 120:
+# #     print("True")
+# # print("Exit")
+
+# if b%2 ==0 :
+#     print("Even")
+# else :
+#     print("ODD")
+
+# a = int(input("Enter a First number "))
+
+# b = int(input("Enter a Second number "))
+
+
+# if a > b :
+#     print("A is greater than b ")
+# if a == b : 
+#     print("A is equal b ")
+# else:
+#     print("b is greater than a ")
+
+
+# a = int(input("Enter a First number "))
+# b = int(input("Enter a Second number "))
+# c = int(input("Enter a THird number "))
+
+
+# if a > b:
+#     if a >c:
+#         print("A is the Largest of Three")
+#         if b > c:
+#             print("C is the smallest")
+        
+            
+# a = 1134
+# b = 1145
+# c = 1137
+# a1 = "largest value"
+# a2 = "middle  value"
+# a3 = "smallest  value"
+# # DEc order me krna n 
+# if a >b & a >c:
+#     a1 = a
+# if b > c:
+#     a2 = b 
+# else: a3 = c
+
+# print( a1 , a2 , a3)
+    
+    
+    
+# marks = int(input("Enter Marks :"))
+
+# if marks >= 80 & marks <= 100:
+#     print("Grade is O ")
+# elif marks >= 70 & marks <=79:
+#     print("Grade is a+ ")
+# elif marks >= 60 & marks <=69:
+#     print("Grade is a ")
+# elif marks >= 55 & marks <=59:
+#     print("Grade is b+ ")
+# elif marks >= 50 & marks <=54:
+#     print("Grade is b ")
+# elif marks >= 45 & marks <=49:
+#     print("Grade is c ")
+# elif marks >= 40 & marks <=44:
+#     print("Grade is p ")""
+    
+    
+    
+
+
+
+
+
+
+
